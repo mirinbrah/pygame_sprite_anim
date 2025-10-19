ba6329c3c44454b85530d594d8d2ffa9a1f686c2
Mikhail Ryabchikov <Nolifegonnamad@gmail.com>
2025-10-19 08:26:16 +0400

    basic anim added

--- main.py
+++ main.py
@@ -4,10 +4,30 @@ import sys
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos_x, pos_y):
         super().__init__()
-        self.image = pygame.Surface([20,20])
-        self.image.fill(pygame.Color('red'))
+
+        self.sprites = []
+        self.sprites.append(pygame.image.load('src/enemy/attack/enemy_attack_0.png'))
+        self.sprites.append(pygame.image.load('src/enemy/attack/enemy_attack_1.png'))
+        self.sprites.append(pygame.image.load('src/enemy/attack/enemy_attack_2.png'))
+        self.sprites.append(pygame.image.load('src/enemy/attack/enemy_attack_3.png'))
+        self.sprites.append(pygame.image.load('src/enemy/attack/enemy_attack_4.png'))
+        self.sprites.append(pygame.image.load('src/enemy/attack/enemy_attack_5.png'))
+        self.sprites.append(pygame.image.load('src/enemy/attack/enemy_attack_6.png'))
+        self.current_sprite = 0
+        self.image = self.sprites[self.current_sprite]
+
         self.rect = self.image.get_rect()
+        # self.rect.x = pos_x
+        # self.rect.y = pos_y
         self.rect.topleft = (pos_x, pos_y)
+
+    def update(self):
+        self.current_sprite += 1
+
+        if self.current_sprite >= len(self.sprites):
+            self.current_sprite = 0
+
+        self.image = self.sprites[self.current_sprite]
 
 
 pygame.init()
@@ -19,8 +39,10 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Sprite Animation')
 
 moving_sprites = pygame.sprite.Group()
-player = Player(100,100)
-moving_sprites.add(player)
+player = Player(130, 120)
+moving_sprites.add(player) # type: ignore
+
+
 
 while True:
     for event in pygame.event.get():
@@ -30,5 +52,6 @@ while True:
 
     screen.fill((0,0,0))
     moving_sprites.draw(screen)
+    moving_sprites.update()
     pygame.display.flip()
     clock.tick(60)
